af5a4d741625902a323578835d32cbf8044459ba
ukurekar <utkarsha.kurekar@gmail.com>
2017-07-06 14:35:24 +0530

    update read.me

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,41 +1,13 @@
-import {
-  Component,
-  OnInit,    
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router }  from '@angular/router';
 
 @Component({
   selector: 'about',
-  styles: [`
-  `],
-//   template: `
-//     <div class="container-fluid">
-//         <div class="row">
-//         </div>
-//         <div class="row">
-//             <div class="col-md-12">
-
-//             </div>
-//         </div>            
-//         <div class="row">
-//             <div class="col-md-12">
-//                 <input type="text"
-//                     (change)="onChangeEmail($event.target.value)"/>
-//                 <div>{{loginState?.errors?.email}}</div>                    
-//                 <button type="button"
-//                     class="btn btn-primary" 
-//                     (click)="onLogin()">
-//                     Continue
-//                 </button>
-//             </div>
-//         </div>            
-//     </div>            
-//   `
-    templateUrl: 'LoginPage.html'
+  templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
 
-  public loginState: any = {
+  loginState: any = {
       email: '',
       errors: {
           email: null
@@ -46,9 +18,7 @@ export class LoginComponent implements OnInit {
 
   }
 
-  public ngOnInit() {
-        
-  }  
+  ngOnInit() {}
 
   onChangeEmail(email: string) {
       this.loginState.email = email;
@@ -61,14 +31,14 @@ export class LoginComponent implements OnInit {
     //check if the email is valid
     if (this.validateEmail()) {
         //move to the next screen
-        this.router.navigate(['main']);        
+        this.router.navigate(['main']);
     } else {
         //throw login error
         this.loginState.errors.email = "Please enter a valid email";
     }
   }
 
-  validateEmail() {      
+  validateEmail() {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(this.loginState.email);
   }
